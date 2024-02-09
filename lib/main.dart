5cce1cd034a23f99904c692189185e9e0d4b989c
Abderrahmane <abderrahmanekihal5@gmail.com>
2024-02-10 00:52:23 +0100

    initialize firebase in both ios and android plateformes of the app

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,21 @@
 
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart' as pathProvider;
 import 'package:weekly_planner/mainScreen.dart';
 
+import 'firebase_options.dart';
 import 'models/user_model.dart';
 import 'models/user_picture.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Directory directory = await pathProvider.getApplicationDocumentsDirectory();
   Hive.init(directory.path);
 
